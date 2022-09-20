9dd9e495e42ab04c7b1acaf093f6e79fe298a222
Alexey-Yoshimitsu <55356806+Alexey-Yoshimitsu@users.noreply.github.com>
2022-09-21 01:39:46 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -270,21 +270,43 @@ const timetable = {
 
 let dayNames = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница']
 const date = new Date().toLocaleDateString();
-console.log(date);
+//console.log(date);
 var currentdate = new Date();
-var oneJan = new Date(currentdate.getFullYear(), 0, 1);
-var numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
-var result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7)+1;// номер недели в году
+// флюгегехаймэн
+Date.prototype.getWeek = function(){
+  var day_miliseconds = 86400000,
+      onejan = new Date(this.getFullYear(),0,1,0,0,0),
+      onejan_day = (onejan.getDay()==0) ? 7 : onejan.getDay(),
+      days_for_next_monday = (8-onejan_day),
+      onejan_next_monday_time = onejan.getTime() + (days_for_next_monday * day_miliseconds),
+      // If one jan is not a monday, get the first monday of the year
+      first_monday_year_time = (onejan_day>1) ? onejan_next_monday_time : onejan.getTime(),
+      this_date = new Date(this.getFullYear(), this.getMonth(),this.getDate(),0,0,0),// This at 00:00:00
+      this_time = this_date.getTime(),
+      days_from_first_monday = Math.round(((this_time - first_monday_year_time) / day_miliseconds));
 
+  var first_monday_year = new Date(first_monday_year_time);
+
+  // We add 1 to "days_from_first_monday" because if "days_from_first_monday" is *7,
+  // then 7/7 = 1, and as we are 7 days from first monday,
+  // we should be in week number 2 instead of week number 1 (7/7=1)
+  // We consider week number as 52 when "days_from_first_monday" is lower than 0,
+  // that means the actual week started before the first monday so that means we are on the firsts
+  // days of the year (ex: we are on Friday 01/01, then "days_from_first_monday"=-3,
+  // so friday 01/01 is part of week number 52 from past year)
+  // "days_from_first_monday<=364" because (364+1)/7 == 52, if we are on day 365, then (365+1)/7 >= 52 (Math.ceil(366/7)=53) and thats wrong
+
+  return (days_from_first_monday>=0 && days_from_first_monday<364) ? Math.ceil((days_from_first_monday+1)/7) : 52;
+}
+var result = (new Date()).getWeek()
+//console.log((currentdate.getDay() + 1 + numberOfDays) / 7)
 let dayName = new Intl.DateTimeFormat('ru-RU', {weekday: 'long'}).format()
 document.querySelector(".h2").innerHTML  = `Сегодня ${dayName}, ${date}`
 const container = document.querySelector("#posts");
 let toDay = currentdate.getDay()-1// день недели в числовом формате от 0-6
+console.log(result)
 
 if (toDay == -1){toDay = 0}
-
-
-console.log(result)
 function createBlock(obj, day) {
   let block = document.createElement('div')
   block.classList.add('block')
